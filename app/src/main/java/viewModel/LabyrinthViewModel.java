3943b80a68a8bcfcc37447323da10fbb5646b50c
Manel Benavides <many1494@gmail.com>
2018-06-11 02:34:45 +0200

    Todo acabado funcional

--- app/src/main/java/viewModel/LabyrinthViewModel.java
+++ app/src/main/java/viewModel/LabyrinthViewModel.java
@@ -4,7 +4,6 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
-import android.util.Log;
 
 import model.Communication.Repository;
 import model.Constants;
